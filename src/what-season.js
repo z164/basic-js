22660eaeba6c5c1b9672c39a7b0fefaac737e96b
z164 <zigrainchan@gmail.com>
2020-10-22 16:06:42 +0300

    feat: add what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  throw new CustomError('Not implemented');
+  if(arguments.length === 0)
+  {
+    return 'Unable to determine the time of year!'
+  }
+  if(toString.call(date) !== '[object Date]')
+  {
+    throw new Error('Not date')
+  }
+  const month = date.getMonth()
+  if(month < 2 || month === 11)
+  {
+    return 'winter'
+  }
+  else if (month < 5)
+  {
+    return 'spring'
+  }
+  else if (month < 8)
+  {
+    return 'summer'
+  }
+  else if (month < 11)
+  {
+    return 'fall'
+  }
 };
