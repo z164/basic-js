1ed21003e476e3c4dfd602ea92fe0a6ecc894153
z164 <zigrainchan@gmail.com>
2020-10-27 11:45:23 +0300

    feat: add repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options = {
+    repeatTimes: 0,
+    separator: '+',
+    addition: '',
+    additionRepeatTimes: 0,
+    additionSeparator: '|'
+}) {
+    if (options.separator === undefined)
+        options.separator = '+'
+    if (options.addition === undefined)
+        options.addition = ''
+    if (options.additionSeparator === undefined)
+        options.additionSeparator = '|'
+    if (options.repeatTimes === undefined)
+        options.repeatTimes = 1
+    if (options.additionRepeatTimes === undefined)
+        options.additionRepeatTimes = 1
+    str = String(str);
+    options.addition = String(options.addition)
+    const strDefaultValue = str;
+    for (let i = 0; i < options.repeatTimes; i++) {
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            str += options.addition;
+            str += options.additionSeparator;
+        }
+        str = str.slice(0, -options.additionSeparator.length)
+        str += options.separator;
+        str += strDefaultValue
+    }
+    str = str.slice(0, -(options.separator.length + strDefaultValue.length))
+    return str;
 };
-  
