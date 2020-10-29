b694ddf52b9c1211ee645638763d9e6131d0f853
z164 <zigrainchan@gmail.com>
2020-10-29 16:20:20 +0300

    feat: add carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,8 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-    throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+    if (typeof (sampleActivity) == 'string' && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) <= 15) {
+        let k = 0.693 / HALF_LIFE_PERIOD;
+        return Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / k);
+    }
+    return false;
 };
