78b71fdb87a368caf4310a6d603841d98b475d3e
z164 <zigrainchan@gmail.com>
2020-11-01 18:52:48 +0300

    feat: transform-array rework

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,39 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
-function getElement(array, index) {
-    if (array[index] === '--discard-next') {
-        array.splice(index, 1)
-        if (typeof array[index] === 'number') {
-            array[index] = ''
-        }
-    } else if (array[index] === '--discard-prev') {
-        array.splice(index, 1)
-        if (typeof array[index - 1] === 'number') {
-            array[index-1] = ''
-        }
-    } else if (array[index] === '--double-next') {
-        array.splice(index, 1)
-        if (typeof array[index] === 'number') {
-            array.splice(index, 0, array[index])
-        }
-    } else if (array[index] === '--double-prev') {
-        array.splice(index, 1)
-        if (typeof array[index - 1] === 'number') {
-            array.splice(index - 1, 0, array[index - 1])
-        }
-    } else {
-        return 0;
-    }
-    return 1;
-}
-
 module.exports = function transform(arr) {
-    if (!Array.isArray(arr)) {
-        throw new Error('Not an array')
-    }
+  if (Array.isArray(arr)) {
+    let result = [];
+
     for (let i = 0; i < arr.length; i++) {
-        i-=getElement(arr, i);
-    }
-    arr = arr.filter(element => element!=='')
-    return arr
+      switch (arr[i]) {
+        case '--discard-prev':
+          if (result.length !== 0 && arr[i - 2] !== '--discard-next') result.pop();
+          break;
+
+        case '--double-prev':
+          if (i !== 0 && arr[i - 2] !== '--discard-next') result.push(arr[i - 1]);
+          break;
+
+        case '--double-next':
+          if (i < arr.length - 1) result.push(arr[i + 1]);
+          break;
+
+        case '--discard-next':
+          i++;
+          break;
+
+        default:
+          result.push(arr[i]);
+      };
+    };
+
+    return result;
+  } else {
+    throw new Error();
+  }
 };
